271479d863a2233f7c628ae9b335cfca3dea23cf
Elemson Ifeanyi <ielemson@gmail.com>
2024-06-18 14:42:20 +0100

    updated content

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -34,16 +34,16 @@
 
         <!-- Place favicon.ico in the root directory -->
         <!-- CSS here -->
-        <link rel="stylesheet" href="assets/css/bootstrap.min.css">
-        <link rel="stylesheet" href="assets/css/animate.min.css">
-        <link rel="stylesheet" href="assets/css/magnific-popup.css">
-        <link rel="stylesheet" href="assets/css/fontawesome-all.min.css">
-        <link rel="stylesheet" href="assets/css/flaticon.css">
-        <link rel="stylesheet" href="assets/css/odometer.css">
-        <link rel="stylesheet" href="assets/css/swiper-bundle.css">
-        <link rel="stylesheet" href="assets/css/aos.css">
-        <link rel="stylesheet" href="assets/css/default.css">
-        <link rel="stylesheet" href="assets/css/main.css">
+        <link rel="stylesheet" href="{{ asset("assets/css/bootstrap.min.css") }}">
+        <link rel="stylesheet" href="{{ asset("assets/css/animate.min.css") }}">
+        <link rel="stylesheet" href="{{ asset("assets/css/magnific-popup.css") }}">
+        <link rel="stylesheet" href="{{ asset("assets/css/fontawesome-all.min.css") }}">
+        <link rel="stylesheet" href="{{ asset("assets/css/flaticon.css") }}">
+        <link rel="stylesheet" href="{{ asset("assets/css/odometer.css") }}">
+        <link rel="stylesheet" href="{{ asset("assets/css/swiper-bundle.css") }}">
+        <link rel="stylesheet" href="{{ asset("assets/css/aos.css") }}">
+        <link rel="stylesheet" href="{{ asset("assets/css/default.css") }}">
+        <link rel="stylesheet" href="{{ asset("assets/css/main.css") }}">
     </head>
 </head>
 <html class="no-js" lang="en">
@@ -52,27 +52,20 @@
     </div>
 </body>
  <!-- JS here -->
- <script src="assets/js/vendor/jquery-3.6.0.min.js"></script>
- <script src="assets/js/bootstrap.bundle.min.js"></script>
- <script src="assets/js/jquery.magnific-popup.min.js"></script>
- <script src="assets/js/jquery.odometer.min.js"></script>
- <script src="assets/js/jquery.appear.js"></script>
- <script src="assets/js/gsap.js"></script>
- <script src="assets/js/ScrollTrigger.js"></script>
- <script src="assets/js/SplitText.js"></script>
- <script src="assets/js/gsap-animation.js"></script>
- <script src="assets/js/jquery.parallaxScroll.min.js"></script>
- <script src="assets/js/swiper-bundle.js"></script>
- <script src="assets/js/ajax-form.js"></script>
- <script src="assets/js/wow.min.js"></script>
- <script src="assets/js/aos.js"></script>
- <script src="assets/js/main.js"></script>
- {{-- <script>
-     const text = document.querySelector('.circle');
-     text.innerHTML = text.textContent.replace(/\S/g, "<span>$&</span>");
-     const element = document.querySelectorAll('.circle span');
-     for (let i = 0; i < element.length; i++) {
-         element[i].style.transform = "rotate(" + i * 17 + "deg)"
-     }
- </script> --}}
+ <script src="{{ asset("assets/js/vendor/jquery-3.6.0.min.js") }}"></script>
+ <script src="{{ asset("assets/js/bootstrap.bundle.min.js") }}"></script>
+ <script src="{{ asset("assets/js/jquery.magnific-popup.min.js") }}"></script>
+ <script src="{{ asset("assets/js/jquery.odometer.min.js") }}"></script>
+ <script src="{{ asset("assets/js/jquery.appear.js") }}"></script>
+ <script src="{{ asset("assets/js/gsap.js") }}"></script>
+ <script src="{{ asset("assets/js/ScrollTrigger.js") }}"></script>
+ <script src="{{ asset("assets/js/SplitText.js") }}"></script>
+ <script src="{{ asset("assets/js/gsap-animation.js") }}"></script>
+ <script src="{{ asset("assets/js/jquery.parallaxScroll.min.js") }}"></script>
+ <script src="{{ asset("assets/js/swiper-bundle.js") }}"></script>
+ <script src="{{ asset("assets/js/ajax-form.js") }}"></script>
+ <script src="{{ asset("assets/js/wow.min.js") }}"></script>
+ <script src="{{ asset("assets/js/aos.js") }}"></script>
+ <script src="{{ asset("assets/js/main.js") }}"></script>
+
 </html>
